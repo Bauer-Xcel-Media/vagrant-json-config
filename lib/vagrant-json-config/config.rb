2d41ba328f0f43a725eea030fea2e65a9efeea28
Jan Schumann <js@schumann-it.com>
2015-11-24 23:44:36 +0100

    added deprecation warnings

--- lib/vagrant-json-config/config.rb
+++ lib/vagrant-json-config/config.rb
@@ -3,11 +3,35 @@ require 'pathname'
 module VagrantPlugins
   module JsonConfig
     class Config < Vagrant.plugin("2", :config)
+
+      # The complete data as JSON object
+      #
+      # @deprecated use function get(key) instead
+      #
+      # @return Hash
       attr_accessor :data
+
       def initialize
         @data = UNSET_VALUE
       end
 
+      # Overrides the coresponding attr_accessor
+      #
+      # @deprecated use function get(key) instead
+      #
+      # @return Hash
+      def data
+        puts "DEPRECATION: Accessing data from the root element accessor is deprecated and will be removed soon."
+        puts "Please use get(key) instead."
+        @data
+      end
+
+      # load json data from file and extract data for the given key
+      # if no key is given all the data will be loaded
+      # by calling this multiple times, every subsequent data
+      # will be merged in
+      #
+      # @raises an error if required is true but the file doeas not exists
       def load_json(file, key = nil, required = false)
         path = Pathname.new file
 
@@ -39,6 +63,11 @@ module VagrantPlugins
         end
       end
 
+      # get data from a given key
+      # raises an error, if the key does not exist
+      # call has?(key) to test if the key exists
+      #
+      # @return Hash
       def get(key)
         unless self.has?(key)
           raise Vagrant::Errors::PluginLoadError, message: "The key #{key} does not exist in ."
@@ -47,6 +76,9 @@ module VagrantPlugins
         @data[key]
       end
 
+      # test if a given key exists within the loaded data
+      #
+      # @return boolean
       def has?(key)
         @data.has_key?(key)
       end
