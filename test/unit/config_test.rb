9eb712045a69ed6605026932e58e89f43908b67c
Jan Schumann <js@schumann-it.com>
2015-11-24 22:26:51 +0100

    added test for merging multiple loaded json data files

--- test/unit/config_test.rb
+++ test/unit/config_test.rb
@@ -4,7 +4,7 @@ require "vagrant-json-config/config"
 
 describe VagrantPlugins::JsonConfig::Config do
   let(:json) { '{"foo":{"bar":"baz"}}' }
-  let(:exsiting_key) { 'foo' }
+  let(:existing_key) { 'foo' }
   let(:non_existing_key) { 'missing' }
 
   let(:unset_value) { described_class.const_get("UNSET_VALUE") }
@@ -12,6 +12,7 @@ describe VagrantPlugins::JsonConfig::Config do
   before do
     Dir.chdir ENV["VAGRANT_CWD"]
     File.open('test.json', 'w') { |file| file.write(json) }
+    File.open('test2.json', 'w') { |file| file.write('{"foo":{"foo":"faz"}}' ) }
   end
 
   subject { described_class.new }
@@ -21,8 +22,8 @@ describe VagrantPlugins::JsonConfig::Config do
   end
 
   describe "loading json data" do
-    it "raises an error if a given file does not exsit" do
-      expect{subject.load_json "missing.json"}.to raise_error
+    it "raises an error if a given file does not exist allthough required" do
+      expect{subject.load_json "missing.json", true}.to raise_error
     end
 
     it "accepts absolute path" do
@@ -38,20 +39,27 @@ describe VagrantPlugins::JsonConfig::Config do
     end
 
     it "raises an error if a non existing key is given" do
-      expect{subject.load_json "test.json", non_existing_key}.to raise_error
+      expect{subject.load_json "test.json", true, non_existing_key}.to raise_error
     end
 
     it "selects data correctly when an existing key is given" do
-      subject.load_json "test.json", exsiting_key
+      subject.load_json "test.json", true, existing_key
 
       expect(JSON.dump(subject.data)).to eq('{"bar":"baz"}')
     end
 
+    it "merges json data when load_json is called multiple times" do
+      subject.load_json "test.json", true, existing_key
+      subject.load_json "test2.json", true
+
+      expect(JSON.dump(subject.data)).to eq('{"bar":"baz","foo":{"foo":"faz"}}')
+    end
+
   end
 
-  describe "retriving json data" do
+  describe "retrieving json data" do
     before do
-      subject.load_json "test.json", exsiting_key
+      subject.load_json "test.json", true, existing_key
     end
 
     it "loads correct data" do
@@ -61,7 +69,7 @@ describe VagrantPlugins::JsonConfig::Config do
     end
 
     it "raises an error on non exsiting key" do
-      expect{subject.get non_existing_key}.to raise_error
+      expect{subject.get "sdcsd"}.to raise_error
     end
   end
 
